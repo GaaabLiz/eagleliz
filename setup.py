8cf005d05d8e9314dbf40752d62b3dddfcbe2a2c
gabliz <lizzosgabriele@gmail.com>
2024-11-17 12:05:09 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Eagle.cool python api.'
+LONG_DESCRIPTION = 'Updated python api for Eagle.Cool app.'
+
+# Setting up
+setup(
+    name="eagleliz",
+    version=VERSION,
+    author="Gabliz",
+    author_email="",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'eagle.cool', 'media', 'api'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
