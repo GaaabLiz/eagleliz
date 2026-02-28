468cbe8df38130edada7a56516c85f91278bd58f
gabliz <lizzosgabriele@gmail.com>
2026-02-28 11:40:55 +0100

    bump: Bump version to 0.0.18

--- eagleliz/project.py
+++ eagleliz/project.py
@@ -1,5 +1,5 @@
 name = 'eagleliz'
-version = '0.0.17'
+version = '0.0.18'
 description = 'Eagle.cool related utility scripts.'
 requires_python = '>=3.12'
 authors = [('Gabliz', 'gabliz.dev@gmail.com')]
