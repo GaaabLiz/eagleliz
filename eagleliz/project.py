1fcddc93db0c542fa4d698145e1a138e7dbcbcf1
gabliz <lizzosgabriele@gmail.com>
2026-03-15 20:58:33 +0100

    bump: Bump version to 0.0.20

--- eagleliz/project.py
+++ eagleliz/project.py
@@ -1,11 +1,5 @@
-"""
-EagleLiz Project Configuration Variables.
-
-This module holds global metadata variables like versioning and author 
-details for the EagleLiz package.
-"""
 name = 'eagleliz'
-version = '0.0.19'
+version = '0.0.20'
 description = 'Eagle.cool related utility scripts.'
 requires_python = '>=3.12'
 authors = [('Gabliz', 'gabliz.dev@gmail.com')]
